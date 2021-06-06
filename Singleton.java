ecb67057fe06040cab8b8b4bcb821f3423e5dc8b
Arsen Sayfullin <silver.arsen@gmail.com>
2021-06-06 18:30:54 +0300

    Add Singleton

---
+++ Singleton.java
@@ -0,0 +1,12 @@
+package Architect;
+
+public class Singleton {
+    private static Singleton instance;
+
+    public static synchronized Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
